ec001e82a230b2025ca0e043692148b0723c597f
Max Röhrl <max.roehrl11@gmail.com>
2020-05-06 14:35:44 +0200

    Add voting functionality

--- app/components/Votes.vue
+++ app/components/Votes.vue
@@ -3,14 +3,30 @@
                  justifyContent="center"
                  alignSelf="center"
                  alignContent="center"
-                 style="width: 15%">
-    <!--Up-->
-    <Label :text="getScore(post.score)" class="vote-label" />
-    <!--Down-->
+                 width="15%">
+    <Ripple rippleColor="#53ba82"
+            class="chevron"
+            @tap="vote(true)">
+      <Image src="res://ic_chevron_up_white_18dp"
+             loadMode="async"
+             :tintColor="getColor(true)" />
+    </Ripple>
+    <Label :text="getScore(post.score)"
+           class="vote-label"
+           :style="{color: getColor()}" />
+    <Ripple rippleColor="#53ba82"
+            class="chevron"
+            @tap="vote(false)">
+      <Image src="res://ic_chevron_down_white_18dp"
+             loadMode="async"
+             :tintColor="getColor(false)" />
+    </Ripple>
   </FlexboxLayout>
 </template>
 
 <script>
+import Reddit from '../services/Reddit';
+
 export default {
   name: 'Votes',
   props: {
@@ -19,13 +35,30 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-    };
-  },
   methods: {
     getScore(score) {
       return score >= 10000 ? (score / 1000).toFixed(1) + 'k' : score;
+    },
+
+    getColor(up) {
+      if (this.post.likes === up || (up === undefined && this.post.likes !== null)) {
+        return this.post.likes ? '#53ba82' : '#bf5826';
+      } else {
+        return '#b8b8b8';
+      }
+    },
+
+    vote(up) {
+      let dir = up ? 1 : -1;
+      if (this.post.likes === up) {
+        this.post.likes = null;
+        this.post.score -= dir;
+        dir = 0;
+      } else {
+        this.post.likes = up;
+        this.post.score += dir;
+      }
+      Reddit.vote(this.post.name, dir);
     },
   },
 };
@@ -33,8 +66,11 @@ export default {
 
 <style scoped>
   .vote-label {
-    color: #53ba82;
-    width: 100%;
     text-align: center;
+    height: 100px;
+  }
+
+  .chevron {
+    height: 90px;
   }
 </style>
