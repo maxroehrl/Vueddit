afe33de90e34955c6e18da1bffd2e5a5c1356086
Max Röhrl <max.roehrl11@gmail.com>
2020-05-24 15:39:42 +0200

    Add big previews to user posts

--- app/components/User.vue
+++ app/components/User.vue
@@ -11,6 +11,7 @@
                  loadOnDemandMode="Auto"
                  loadOnDemandBufferSize="5"
                  pullToRefresh="true"
+                 :itemTemplateSelector="templateSelector"
                  @loadMoreDataRequested="onLoadMorePostsRequested"
                  @pullToRefreshInitiated="onPullDown">
       <v-template name="header">
@@ -19,7 +20,45 @@
                       selectedBackgroundColor="#53ba82"
                       @selectedIndexChange="onSortingChange" />
       </v-template>
-      <v-template>
+      <v-template name="big">
+        <StackLayout>
+          <FlexboxLayout flexDirection="row"
+                         alignSelf="flex-start"
+                         justifyContent="flex-start">
+            <Votes :post="post" width="15%" />
+            <Ripple rippleColor="#53ba82"
+                    width="85%"
+                    @longPress="onLongPress(post)"
+                    @tap="openComments(post)">
+              <Label textWrap="true">
+                <FormattedString>
+                  <Span :style="{'background-color': post.link_flair_background_color || defaultFlairColor}"
+                        :text="post.link_flair_text"
+                        class="post-flair" />
+                  <Span :text="post.link_flair_text ? ' ' + post.title : post.title"
+                        class="post-title"
+                        :style="{color: post.stickied ? '#53ba82' : 'white'}" />
+                  <Span :text="' (' + post.domain + ') \n'" class="post-domain" />
+                  <Span :text="post.over_18 ? 'nsfw ' : ''" class="post-nsfw" />
+                  <Span :text="post.spoiler ? 'spoiler ' : ''" class="post-spoiler" />
+                  <Span :text="post.num_comments + ' comments '" class="post-num-comments" />
+                  <Span :text="post.subreddit" class="post-subreddit" />
+                </FormattedString>
+              </Label>
+            </Ripple>
+          </FlexboxLayout>
+          <Ripple rippleColor="#53ba82"
+                  width="100%"
+                  @longPress="onLongPress(post)"
+                  @tap="openComments(post)">
+            <Image :src="getImage(post)"
+                   stretch="aspectFit"
+                   class="post-image"
+                   loadMode="async" />
+          </Ripple>
+        </StackLayout>
+      </v-template>
+      <v-template name="small">
         <FlexboxLayout flexDirection="row"
                        alignSelf="flex-start"
                        justifyContent="flex-start">
@@ -39,7 +78,7 @@
                         class="post-flair" />
                   <Span :text="post.link_flair_text ? ' ' + post.title : post.title"
                         class="post-title"
-                        :style="{color: post.stickied && subreddit && subreddit.created ? '#53ba82' : 'white'}" />
+                        :style="{color: post.stickied ? '#53ba82' : 'white'}" />
                   <Span :text="' (' + post.domain + ') \n'" class="post-domain" />
                   <Span :text="post.over_18 ? 'nsfw ' : ''" class="post-nsfw" />
                   <Span :text="post.spoiler ? 'spoiler ' : ''" class="post-spoiler" />
@@ -61,13 +100,13 @@
 </template>
 
 <script>
-import Votes from './Votes';
-import Reddit from '../services/Reddit';
 import {SegmentedBarItem} from 'tns-core-modules/ui/segmented-bar';
 import {ObservableArray} from 'tns-core-modules/data/observable-array';
-import Comments from './Comments';
 import {action} from 'tns-core-modules/ui/dialogs';
 import {LoadingIndicator, Mode} from '@nstudio/nativescript-loading-indicator';
+import Reddit from '../services/Reddit';
+import Comments from './Comments';
+import Votes from './Votes';
 
 export default {
   name: 'User',
@@ -102,6 +141,15 @@ export default {
     };
   },
   methods: {
+    templateSelector(item, index, items) {
+      const subHasMoreThanHalfPictures = items
+          .slice(0, 10)
+          .map((post) => Boolean(post && post.preview && post.preview.images && post.preview.images.length))
+          .reduce((a, b) => a + b, 0) > 5;
+      const itemHasPreview = Boolean(Reddit.getPreview(item, 300, false));
+      return subHasMoreThanHalfPictures && itemHasPreview ? 'big' : 'small';
+    },
+
     onPullDown(args) {
       this.refresh().finally(() => args.object.notifyPullToRefreshFinished(true));
     },
@@ -140,10 +188,11 @@ export default {
       return Reddit.getPreview(post);
     },
 
+    getImage(post) {
+      return Reddit.getImage(post);
+    },
+
     openComments(post) {
-      // explode (Android Lollipop(21) and up only), fade,
-      // flip (same as flipRight), flipRight, flipLeft,
-      // slide (same as slideLeft), slideLeft, slideRight, slideTop, slideBottom
       this.$navigateTo(Comments, {
         transition: 'slide',
         props: {
