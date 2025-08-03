4a13568058287dbb4ebe99dc4a41a66b80a93965
Max Röhrl <max.roehrl11@gmail.com>
2025-08-03 23:00:20 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.android.tools.build:gradle:8.10.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.0")
     }
 }
